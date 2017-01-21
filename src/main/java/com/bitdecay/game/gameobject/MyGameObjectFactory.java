032aa4d4162e66df20008718b9d0016061e8964d
mwingfield <mwingfie@gmail.com>
2017-01-21 15:45:19 -0700

    make rotations work reliably

--- src/main/java/com/bitdecay/game/gameobject/MyGameObjectFactory.java
+++ src/main/java/com/bitdecay/game/gameobject/MyGameObjectFactory.java
@@ -17,16 +17,16 @@ public final class MyGameObjectFactory {
 
     public static MyGameObject ship(){
         MyGameObject t = new MyGameObject();
-        t.addComponent(new PlayerInputComponent(t, 0.75f));
+        t.addComponent(new PlayerInputComponent(t, 1f));
         t.addComponent(new DebugCircleComponent(t, com.badlogic.gdx.graphics.Color.GREEN, 25));
         t.addComponent(new PositionComponent(t, 10, 20));
         t.addComponent(new RotationComponent(t, 0));
-        t.addComponent(new DesiredDirectionComponent(t, 0, 0.01f));
-        t.addComponent(new SizeComponent(t, 49, 26));
+        t.addComponent(new DesiredDirectionComponent(t, 0, 0.1f));
+        t.addComponent(new SizeComponent(t, 49 * 4, 26 * 4));
         t.addComponent(new OriginComponent(t));
         t.addComponent(new CameraFollowComponent(t));
         t.addComponent(new PredictiveCameraFollowComponent(t));
-        t.addComponent(new VelocityComponent(t,1,0));
+        t.addComponent(new VelocityComponent(t,0.01f,0));
         t.addComponent(new StaticImageComponent(t, "player/sub"));
         t.addComponent(new CollisionComponent(t));
         t.addComponent(new AccelerationComponent(t));
