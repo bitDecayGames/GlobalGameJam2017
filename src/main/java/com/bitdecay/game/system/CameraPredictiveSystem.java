4be3ebd3f92f4ad893d9d71ad262b78515e0de4b
mwingfield <mwingfie@gmail.com>
2017-01-22 02:58:42 -0700

    change the camera so that it has a top and bottom boundary

--- src/main/java/com/bitdecay/game/system/CameraPredictiveSystem.java
+++ src/main/java/com/bitdecay/game/system/CameraPredictiveSystem.java
@@ -1,19 +1,18 @@
 package com.bitdecay.game.system;
 
+import com.badlogic.gdx.math.Vector2;
 import com.bitdecay.game.component.PositionComponent;
 import com.bitdecay.game.component.PredictiveCameraFollowComponent;
-import com.bitdecay.game.component.RotationComponent;
-import com.bitdecay.game.component.VelocityComponent;
 import com.bitdecay.game.gameobject.MyGameObject;
 import com.bitdecay.game.room.AbstractRoom;
 import com.bitdecay.game.system.abstracted.AbstractForEachUpdatableSystem;
-import com.bitdecay.game.util.VectorMath;
 
 /**
  * This system is in charge of updating the camera with the list of points to follow each step.
  */
 public class CameraPredictiveSystem extends AbstractForEachUpdatableSystem {
     private float leadDistance = 0;
+    private Vector2 right = new Vector2(1, 0);
     public CameraPredictiveSystem(AbstractRoom room, float leadDistance) {
         super(room);
         this.leadDistance = leadDistance;
@@ -21,15 +20,12 @@ public class CameraPredictiveSystem extends AbstractForEachUpdatableSystem {
 
     @Override
     protected boolean validateGob(MyGameObject gob) {
-        return gob.hasComponents(PredictiveCameraFollowComponent.class, PositionComponent.class, VelocityComponent.class);
+        return gob.hasComponents(PredictiveCameraFollowComponent.class, PositionComponent.class);
     }
 
     @Override
     protected void forEach(float delta, MyGameObject gob) {
-        // room.camera.addFollowPoint(pos.toVector2())
-        gob.forEachComponentDo(PositionComponent.class, pos ->
-                        gob.forEachComponentDo(VelocityComponent.class, velocity ->
-                                room.camera.addFollowPoint(velocity.toVector2().nor().scl(leadDistance).add(pos.toVector2()))));
+        gob.forEachComponentDo(PositionComponent.class, pos -> room.camera.addFollowPoint(right.cpy().scl(leadDistance).add(pos.toVector2())));
     }
 
 }
