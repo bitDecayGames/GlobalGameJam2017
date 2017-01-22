cafcea32fde9ecd242c759d28a37fd3bdd8846d1
Logan Moore <monday.hopscotch@gmail.com>
2017-01-21 23:11:34 -0700

    shrink light. tune player speed/turning

--- src/main/java/com/bitdecay/game/component/ProximityIlluminationComponent.java
+++ src/main/java/com/bitdecay/game/component/ProximityIlluminationComponent.java
@@ -6,8 +6,8 @@ import com.bitdecay.game.gameobject.MyGameObject;
  * Created by Monday on 1/21/2017.
  */
 public class ProximityIlluminationComponent extends AbstractComponent {
-    public float fullBrightnessRadius = 100;
-    public float fadeRange = 100;
+    public float fullBrightnessRadius = 10;
+    public float fadeRange = 150;
 
     public ProximityIlluminationComponent(MyGameObject obj) {
         super(obj);
