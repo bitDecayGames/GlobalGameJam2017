545d6d62c517d2c47c90bee3fdcc5a5dc034cb8d
mwingfield <mwingfie@gmail.com>
2017-01-22 12:13:40 -0700

    move kraken and add quit to main menu

--- src/main/java/com/bitdecay/game/component/KrakenComponent.java
+++ src/main/java/com/bitdecay/game/component/KrakenComponent.java
@@ -9,10 +9,18 @@ public class KrakenComponent extends AbstractComponent {
 
     public MyGameObject player;
     public MyGameObject face;
+    public MyGameObject leftEye;
+    public MyGameObject rightEye;
     public MyGameObject head;
     public MyGameObject leftTentacle;
     public MyGameObject rightTentacle;
     public MyGameObject shortTentacles;
+
+    public float acceleration = 0.01f;
+    public float direction = acceleration;
+    public float maxSpeed = 1;
+    public float speed = maxSpeed;
+    public float boundary = 20;
 
     public KrakenComponent(MyGameObject obj){super(obj);}
 }
---
+++ src/main/java/com/bitdecay/game/system/KrakenSystem.java
@@ -0,0 +1,39 @@
+package com.bitdecay.game.system;
+
+import com.bitdecay.game.component.KrakenComponent;
+import com.bitdecay.game.component.RelativePositionComponent;
+import com.bitdecay.game.gameobject.MyGameObject;
+import com.bitdecay.game.room.AbstractRoom;
+import com.bitdecay.game.system.abstracted.AbstractForEachUpdatableSystem;
+
+public class KrakenSystem extends AbstractForEachUpdatableSystem{
+
+    public KrakenSystem(AbstractRoom room){
+        super(room);
+    }
+
+    @Override
+    protected boolean validateGob(MyGameObject gob) {
+        return gob.hasComponents(KrakenComponent.class);
+    }
+
+    @Override
+    protected void forEach(float delta, MyGameObject gob) {
+        gob.forEachComponentDo(KrakenComponent.class, kraken ->
+                gob.forEachComponentDo(RelativePositionComponent.class, relPos -> {
+                    kraken.speed += kraken.acceleration;
+                    relPos.y += kraken.speed;
+                    System.out.println("" + relPos.y + " " + kraken.direction + " " + kraken.speed);
+                    if (kraken.direction > 0 && kraken.speed > kraken.maxSpeed) kraken.speed = kraken.maxSpeed;
+                    else if(kraken.direction < 0 && kraken.speed < -kraken.maxSpeed) kraken.speed = -kraken.maxSpeed;
+
+                    if (kraken.direction > 0 && relPos.y > kraken.boundary) {
+                        kraken.acceleration *= -1;
+                        kraken.direction = -1;
+                    } else if (kraken.direction < 0 && relPos.y < -kraken.boundary){
+                        kraken.direction = 1;
+                        kraken.acceleration *= -1;
+                    }
+                }));
+    }
+}
